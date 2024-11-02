bd215b7ff08855ad9a6033cb082ab6a70af9c6d8
wesley arizio <wesleyarizio26@gmail.com>
2024-11-02 16:11:21 -0300

    configure database with diesel

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,10 +1,13 @@
+use crate::AppState;
 use actix_web::web;
 
-pub async fn upload() -> String {
+pub async fn upload(_data: web::Data<AppState>) -> String {
+    // TODO - Save file locally
     String::from("Hello World!")
 }
 
 pub async fn csv(path: web::Path<String>) -> String {
-    let id = path.into_inner();
-    format!("Hello {} csv file", id)
+    let _id = path.into_inner();
+    // TODO - Retrieve file by id
+    String::from("Hello World!")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod endpoint;
+pub mod models;
+pub mod schema;
 
-use actix_web::{App, HttpServer, web};
+use actix_web::{web, App, HttpServer};
+use std::sync::Arc;
 
 use clap::Parser;
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::SqliteConnection;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -15,6 +20,30 @@ struct Args {
     /// Port in which the server will run
     #[arg(short, default_value_t = 8080, env = "CSV_SHARE_HUB_PORT")]
     port: u16,
+
+    /// Url to connect to the database
+    #[arg(short, env = "CSV_SHARE_HUB_DATABASE_URL")]
+    db_url: String,
+}
+
+type SqlitePool = Pool<ConnectionManager<SqliteConnection>>;
+
+struct AppState {
+    pool: Arc<SqlitePool>,
+}
+
+impl AppState {
+    pub fn new(pool: Arc<SqlitePool>) -> Self {
+        Self { pool }
+    }
+}
+
+pub fn get_connection_pool(url: &str) -> SqlitePool {
+    let manager = ConnectionManager::<SqliteConnection>::new(url);
+    Pool::builder()
+        .test_on_check_out(true)
+        .build(manager)
+        .expect("Could not build connection pool")
 }
 
 #[actix_web::main]
@@ -26,12 +55,18 @@ async fn main() -> std::io::Result<()> {
 
     let args = Args::parse();
 
-    HttpServer::new(|| App::new().service(
-        web::scope("/csv")
-            .route("/", web::post().to(endpoint::upload))
-            .route("/{id}", web::get().to(endpoint::csv))
-    ))
-        .bind((args.api_host, args.port))?
-        .run()
-        .await
+    let pool = Arc::new(get_connection_pool(&args.db_url));
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState::new(Arc::clone(&pool))))
+            .service(
+                web::scope("/csv")
+                    .route("/", web::post().to(endpoint::upload))
+                    .route("/{id}", web::get().to(endpoint::csv)),
+            )
+    })
+    .bind((args.api_host, args.port))?
+    .run()
+    .await
 }
